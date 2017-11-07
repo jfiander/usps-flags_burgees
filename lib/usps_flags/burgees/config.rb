996e5bf63e94f6d2a2f02d943f7c34c7f4d6e21d
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-07 17:55:35 -0500

    Fix extension for USPSFlags::Config

---
+++ lib/usps_flags/burgees/config.rb
@@ -0,0 +1,17 @@
+class USPSFlags::Config
+  @@burgees_dir ||= if defined?(::Rails)
+    "#{::Rails.root}/app/lib/usps-burgees"
+  else
+    "#{File.dirname(__dir__)}/output"
+  end
+
+  attr_accessor :burgees_dir
+
+  alias :parent_initialize :initialize
+  def initialize
+    @burgees_dir = @@burgees_dir
+    parent_initialize
+    yield self if block_given?
+    @@burgees_dir = @burgees_dir
+  end
+end
