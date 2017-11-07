e55c06dae1e6d4009601ec5e6d2caaa71d59649c
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-07 18:08:37 -0500

    Make available a class method

--- lib/usps_flags/burgees.rb
+++ lib/usps_flags/burgees.rb
@@ -6,7 +6,7 @@ class USPSFlags::Burgees
   require 'usps_flags/burgees/builtins'
   require 'usps_flags/burgees/customs'
 
-  def available
+  def self.available
     USPSFlags::Burgees::Builtins.available + USPSFlags::Burgees::Customs.available
   end
 end
