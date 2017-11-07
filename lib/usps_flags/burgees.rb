d497ea0acde00ee58f7d7247db8d653d8140f449
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-07 17:54:11 -0500

    Fix requires and available links

--- lib/usps_flags/burgees.rb
+++ lib/usps_flags/burgees.rb
@@ -1,11 +1,11 @@
+require 'usps_flags'
+require 'usps_flags/burgees/config'
+
 class USPSFlags::Burgees
-  require 'usps_flags/config'
   require 'usps_flags/burgees/builtins'
   require 'usps_flags/burgees/customs'
 
   def available
-    Dir.glob('lib/usps_flags/burgees/builtins/**/*.rb').map do |b|
-      b.split("/").last.split(/\.rb$/).first.to_sym
-    end
+    USPSFlags::Burgees::Builtins.available + USPSFlags::Burgees::Customs.available
   end
 end
