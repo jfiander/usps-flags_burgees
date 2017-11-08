484d01fc326c8804e71525acd8d416781f9a8c6e
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-07 19:56:03 -0500

    Refactor Burgees to instantiate

--- lib/usps_flags/burgees.rb
+++ lib/usps_flags/burgees.rb
@@ -6,56 +6,62 @@ class USPSFlags::Burgees
   require 'usps_flags/burgees/builtins'
   require 'usps_flags/burgees/customs'
 
-  class << self
-    def available
-      USPSFlags::Burgees::Builtins.available + USPSFlags::Burgees::Customs.available
+  def self.available
+    USPSFlags::Burgees::Builtins.available + USPSFlags::Burgees::Customs.available
+  end
+
+  def initialize
+    @squadron = nil
+    @outfile = nil
+    # @width = 3000
+    # @height = 2000
+    @title = nil
+    @generated_at = Time.now.strftime("%Y%m%d.%H%S%z")
+    yield self if block_given?
+    @title ||= title(@squadron)
+  end
+
+  attr_accessor :squadron
+  attr_accessor :outfile
+  # attr_accessor :width
+  # attr_accessor :height
+  attr_accessor :title
+
+  def svg
+    raise USPSFlags::Errors::UnknownBurgee unless USPSFlags::Burgees.available.include?(@squadron)
+
+    @svg = <<~SVG
+      #{USPSFlags::Core.headers(title: @title)}
+      #{core(@squadron)}
+      #{USPSFlags::Core.footer}
+    SVG
+
+    USPSFlags::Helpers.output(@svg, outfile: @outfile)
+  end
+
+  private
+  def core(burgee)
+    if custom?(burgee)
+      USPSFlags::Burgees::Customs.get(burgee)
+    elsif builtin?(burgee)
+      USPSFlags::Burgees::Builtins.get(burgee)
     end
+  end
 
-    def get(burgee, outfile: nil)
-      raise USPSFlags::Errors::UnknownBurgee unless available.include?(burgee)
+  def custom?(burgee)
+    USPSFlags::Burgees::Customs.available.include?(burgee)
+  end
 
-      # Configuration values may be editable
-      @width = 3000
-      @height = 2000
-      @view_width = 3000
-      @view_height = 2000
-      @title = title(burgee)
+  def builtin?(burgee)
+    USPSFlags::Burgees::Builtins.available.include?(burgee)
+  end
 
-      @generated_at = Time.now.strftime("%Y%m%d.%H%S%z")
-
-      svg = <<~SVG
-        #{USPSFlags::Core.headers}
-        #{core(burgee)}
-        #{USPSFlags::Core.footer}
-      SVG
-
-      USPSFlags::Helpers.output(svg, outfile: outfile)
-    end
-
-    private
-    def core(burgee)
-      if custom?(burgee)
-        USPSFlags::Burgees::Customs.get(burgee)
-      elsif builtin?(burgee)
-        USPSFlags::Burgees::Builtins.get(burgee)
-      end
-    end
-
-    def custom?(burgee)
-      USPSFlags::Burgees::Customs.available.include?(burgee)
-    end
-
-    def builtin?(burgee)
-      USPSFlags::Burgees::Builtins.available.include?(burgee)
-    end
-
-    def title(burgee)
-      burgee_string = burgee.to_s
-      burgee_title = if burgee.match /_/
-        burgee_string.split("_").map { |word| word.capitalize } + " Burgee"
-      else
-        "#{burgee_string} Burgee"
-      end
+  def title(burgee)
+    burgee_string = burgee.to_s
+    burgee_title = if burgee_string.match(/_/)
+      burgee_string.gsub("_", " ").split(" ").map { |word| word.capitalize }.join(" ") + " Burgee"
+    else
+      "#{burgee_string.capitalize} Burgee"
     end
   end
 end
